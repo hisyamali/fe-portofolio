03b4b0d7f6fa4dfa1161cdbea81d1cbb6a2caf9e
hisyamali <hisyamaliwafi@gmail.com>
2024-11-14 14:44:10 +0700

    router index

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
+import SkillSection from '@/components/SkillSection.vue'
+import ServiceSection from '@/components/ServiceSection.vue'
+import ProjectSection from '@/components/ProjectSection.vue'
+import HireMeSection from '@/components/HireMeSection.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/skills',
+      name: 'skills',
+      component: SkillSection
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/services',
+      name: 'services',
+      component: ServiceSection
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      // component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: ProjectSection
+    },
+    {
+      path: '/hire-me',
+      name: 'hire-me',
+      component: HireMeSection
     }
   ]
 })
